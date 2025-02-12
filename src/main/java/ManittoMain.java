4c6a8174d901b1bc01b6291dca7389b544d32176
icebear0111 <wltjq0901@naver.com>
2025-02-12 11:21:03 +0900

    🐛 fix : 로고 수정

--- src/main/java/ManittoMain.java
+++ src/main/java/ManittoMain.java
@@ -1,9 +1,11 @@
 import controller.UserController;
+import util.AsciiArtUtil;
 
 public class ManittoMain {
 	private static final UserController userController = new UserController();
 
 	public static void main(String[] args) {
+		AsciiArtUtil.printWoorittoLogo(); // 로고 출력
 		userController.run(); // 로그인 또는 회원가입 선택 시작
 	}
 }
